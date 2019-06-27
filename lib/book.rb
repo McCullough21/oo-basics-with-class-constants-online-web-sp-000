b56908c1a055e53f62cc9312b5c44b142c6544c0
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-27 20:53:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,20 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
 
 GENRES = []
 
   def initialize(title)
     @title = title
-    GENRES << genre.new("genre")
   end
+
+def genre=(genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+  GENRES << genre.new("")
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
